dcb8958d2585bd11403b7d4d2eeb529ffbe049bc
Kinnary Raichura <kinnary.raischura@bhnetwork.com>
2015-11-24 10:37:55 -0500

    Removed security since CORS options request need not be authorised. Kept the code available, since might need for future reference

--- src/main/java/com/nexient/nbcu/orbit/config/SecurityConfig.java
+++ src/main/java/com/nexient/nbcu/orbit/config/SecurityConfig.java
@@ -1,28 +1,28 @@
-package com.nexient.nbcu.orbit.config;
-
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-
-/**
- * Created by kraichura on 11/16/2015.
- */
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable()
-                .authorizeRequests()
-                .antMatchers(HttpMethod.POST, "/orbit/order/**").authenticated()
-                .antMatchers(HttpMethod.PUT, "/orbit/order/**").authenticated()
-                .antMatchers(HttpMethod.DELETE, "/orbit/order/**").authenticated()
-                .antMatchers(HttpMethod.POST, "/orbit/deal/**").authenticated()
-                .antMatchers(HttpMethod.PUT, "/orbit/deal/**").authenticated()
-                .antMatchers(HttpMethod.DELETE, "/orbit/deal/**").authenticated()
-                .anyRequest().permitAll()
-                .and()
-                .httpBasic().and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-    }
-}
+//package com.nexient.nbcu.orbit.config;
+//
+//import org.springframework.http.HttpMethod;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.config.http.SessionCreationPolicy;
+//
+///**
+// * Created by kraichura on 11/16/2015.
+// */
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                .csrf().disable()
+//                .authorizeRequests()
+//                .antMatchers(HttpMethod.POST, "/orbit/order/**").authenticated()
+//                .antMatchers(HttpMethod.PUT, "/orbit/order/**").authenticated()
+//                .antMatchers(HttpMethod.DELETE, "/orbit/order/**").authenticated()
+//                .antMatchers(HttpMethod.POST, "/orbit/deal/**").authenticated()
+//                .antMatchers(HttpMethod.PUT, "/orbit/deal/**").authenticated()
+//                .antMatchers(HttpMethod.DELETE, "/orbit/deal/**").authenticated()
+//                .anyRequest().permitAll()
+//                .and()
+//                .httpBasic().and()
+//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+//    }
+//}
